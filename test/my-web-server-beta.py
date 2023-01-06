8e7255428a75d6680a9e8b03b4aa3749da01bf40
sandeepacademe <93729145+sandeepacademe@users.noreply.github.com>
2023-01-06 15:40:07 +0530

    Update my-web-server-beta.py
    updates

--- test/my-web-server-beta.py
+++ test/my-web-server-beta.py
@@ -1,7 +1,7 @@
 from flask import Flask
 from flask import jsonify
 from flask import request
-
+import json
 
 app = Flask(__name__)
 
@@ -10,6 +10,8 @@ about =  {
             "name"    : "my-web-server-beta:1.0.1"
         }
 
+contact = '{"contact": "developer@mywebserver.test"}'
+
 users = []
 
 @app.route('/', methods=['GET'])
@@ -17,8 +19,13 @@ def hello_world():
     return "Hello, my-web-server-beta!"
 
 @app.route('/about', methods=['GET'])
-def returnAll():
+def about_fn():
     return jsonify({'about' : about})
+
+@app.route('/contact', methods=['GET'])
+def contact_fn():
+    json_object = json.loads(contact)
+    return (json.dumps(json_object, indent = 3))
 
 if __name__ == "__main__":
     app.run(debug=True)
